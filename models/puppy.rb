0f9cb2b26c329f1954e7e6f3d97c134a52a78c25
Nicholas Smith <nico.ehs05@gmail.com>
2018-10-09 14:58:50 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,9 +1,7 @@
 class Puppy
   attr_accessor :name, :age, :breed
   
-  def initialize(name, breed, age)
-    @name=name
-    @age=age
-    @breed=breed
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 end
